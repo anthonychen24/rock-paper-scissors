4643594b2cf8f7dcc477f26c86cc495c425e3c82
Anthony Chen <anthonychen2410@gmail.com>
2025-03-31 19:41:54 -0400

    Create the function playRound

--- main.js
+++ main.js
@@ -10,9 +10,26 @@ function getComputerChoice() {
     }else {
         return 'scissors';
     }
-};
+}
 
 function getHumanChoice() {
-    let humanChoice = prompt('Enter rock paper or scissors','');
+    let humanChoice = prompt('Enter rock paper or scissors','')
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanSelection === 'rock' && computerSelection === 'scissors' || humanSelection === 'paper' && computerSelection === 'rock' || humanSelection === 'scissors' && computerSelection === 'paper') {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        } else if (humanSelection === computerSelection) {
+            console.log('Draw.');
+        } else {
+            computerScore++;
+            console.log(`You lose. Computer chose ${computerChoice} which beats ${humanChoice}`);
+        }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+playRound(humanSelection, computerSelection);
