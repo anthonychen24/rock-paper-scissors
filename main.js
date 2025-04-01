7822d85d32bc4371032c217e0638349c5dc67446
Anthony Chen <anthonychen2410@gmail.com>
2025-03-31 20:49:05 -0400

    Create the function playGame

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     if (random === 0) {
@@ -17,19 +14,35 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanSelection === 'rock' && computerSelection === 'scissors' || humanSelection === 'paper' && computerSelection === 'rock' || humanSelection === 'scissors' && computerSelection === 'paper') {
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        } else if (humanSelection === computerSelection) {
-            console.log('Draw.');
-        } else {
-            computerScore++;
-            console.log(`You lose. Computer chose ${computerChoice} which beats ${humanChoice}`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i=0; i<5; i++){
+        function playRound(humanChoice, computerChoice) {
+            if (humanSelection === 'rock' && computerSelection === 'scissors' || humanSelection === 'paper' && computerSelection === 'rock' || humanSelection === 'scissors' && computerSelection === 'paper') {
+                humanScore++;
+                console.log(`You win! ${humanChoice} beats computer choice ${computerChoice}`);
+            } else if (humanSelection === computerSelection) {
+                console.log('Draw.');
+            } else {
+                computerScore++;
+                console.log(`You lose. Computer chose ${computerChoice} which beats ${humanChoice}`);
+            }
         }
+
+        const humanSelection = getHumanChoice().toLowerCase();
+        const computerSelection = getComputerChoice().toLowerCase();
+
+        playRound(humanSelection, computerSelection);
+        console.log(`Score is: User= ${humanScore} | Comp= ${computerScore}`);    
+    }
+    
+    if (humanScore > computerScore){
+        console.log('You won best out of 5 rounds!');
+    }else {
+        console.log('You lost best out of 5 rounds.');
+    }
 }
 
-const humanSelection = getHumanChoice().toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
-
-playRound(humanSelection, computerSelection);
+playGame();
